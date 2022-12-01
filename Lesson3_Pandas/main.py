a51ebb48af25e309b85410cc4896ca93479e3115
wixxxez <vinewood02@gamil.com>
2022-12-01 16:16:36 +0200

    Lesson 3 Pandas answer four

--- Lesson3_Pandas/main.py
+++ Lesson3_Pandas/main.py
@@ -14,9 +14,9 @@ def answer_one():
 df = answer_one()
 
 #print(df)
-def answer_two(df):
+def answer_two():
 
-
+    df = answer_one()
     #df.iloc[2, 10: ].name
     avgGPD = pd.Series([])
     for i in range(len(df)):
@@ -25,9 +25,29 @@ def answer_two(df):
         avg = values.mean()
         avgGPD = avgGPD.append(pd.Series({ country_name : avg }))
 
-    return avgGPD
+    return avgGPD.sort_values(ascending=False)
 
-#print(answer_two(df))
+#print(answer_two())
 
+def answer_three():
+    top15 = answer_one()
+    top15Avg = answer_two();
 
+    countryAvg = top15Avg.iloc[5]
+    countryName = top15Avg[top15Avg == countryAvg].index[0]
 
+    return top15.loc[countryName]["2015"] - top15.loc[countryName]["2006"]
+
+#print(answer_three())
+
+def answer_four():
+    top15 = answer_one()
+    coef = []
+    for i in range(len(top15)):
+        coef.append(top15.iloc[i]["Citations"] / top15.iloc[i]["Self-citations"])
+    top15['coef'] = coef
+    top15 = top15.sort_values(by="coef", ascending=False)
+
+    return (top15.iloc[0].name, top15.iloc[0]["coef"])
+
+print(answer_four())
